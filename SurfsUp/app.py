1a80e2c4f6ae7d8c424a7b54f0d9b6ce79adfc37
Baron Stubblefield <Baron.Stubblefield@kmob.com>
2023-06-19 18:14:52 -0700

    finished part 1

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,30 +1,58 @@
 # Import the dependencies.
+import numpy as np
+import datetime as dt
 
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
 
 
 #################################################
 # Database Setup
 #################################################
-
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
 # reflect an existing database into a new model
+Base = automap_base()
+Base.prepare(engine)
 
 # reflect the tables
-
+Base.metadata.tables
+Base.classes.keys()
 
 # Save references to each table
-
+Measurement = Base.classes.measurement
+Station = Base.classes.station
 
 # Create our session (link) from Python to the DB
-
+session = Session(engine)
 
 #################################################
 # Flask Setup
 #################################################
-
-
-
+app = Flask(__name__)
 
 #################################################
 # Flask Routes
 #################################################
+@app.route("/")
+def index():
+    """List all available routes"""
+    return (
+        f"Available Routes:<br/>"
+        
+    )
+
+@app.route("/api/v1.0/percipitation")
+
+@app.route("/api/v1.0/stations")
+
+@app.route("/api/v1.0/tobs")
+
+@app.route("/api/v1.0/<start>/<stop>")
+
+
+
