f6f5d483befea6a37d8aabc75a0408d548adf42b
Baron Stubblefield <Baron.Stubblefield@kmob.com>
2023-06-20 19:00:10 -0700

    api testing

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -20,7 +20,7 @@ Base = automap_base()
 Base.prepare(engine)
 
 # reflect the tables
-Base.metadata.tables
+# Base.metadata.tables
 Base.classes.keys()
 
 # Save references to each table
@@ -42,6 +42,7 @@ baseRoute = "/api/v1.0"
 route_percip = f"{baseRoute}/percipitation"
 route_stations = f"{baseRoute}/stations"
 route_tobs = f"{baseRoute}/tobs"
+route_start = f"{baseRoute}/<start>"
 route_startstop = f"{baseRoute}/<start>/<stop>"
 
 @app.route("/")
@@ -52,18 +53,84 @@ def index():
         f"{route_percip}<br/>"
         f"{route_stations}<br/>"
         f"{route_tobs}<br/>"
+        f"{route_start}<br/>"
         f"{route_startstop}"
     )
 
 @app.route(route_percip)
 def percip():
-    
+    session = Session(engine)
+    sel = [Measurement.date,Measurement.prcp]
+    queryresult = session.query(*sel).all()
+    session.close()
+
+    precipitation = []
+    for date, prcp in queryresult:
+        prcp_dict = {"Date":date,"Precipitation":prcp}
+        precipitation.append(prcp_dict)
+
+    return jsonify(precipitation)
 
 @app.route(route_stations)
+def stations():
+    session = Session(engine)
+    sel = [Station.station,Station.name,Station.latitude,Station.longitude,Station.elevation]
+    queryresult = session.query(*sel).all()
+    session.close()
+
+    stations = []
+    for station,name,lat,lon,el in queryresult:
+        station_dict = {"Station":station,"Name":name,"Lat":lat,"Lon":lon,"Elevation":el}
+        stations.append(station_dict)
+
+    return jsonify(stations)
 
 @app.route(route_tobs)
+def tobs():
+    session = Session(engine)
+    lateststr = session.query(Measurement.date).order_by(Measurement.date.desc()).first()[0]
+    latestdate = dt.datetime.strptime(lateststr, '%Y-%m-%d')
+    querydate = dt.date(latestdate.year -1, latestdate.month, latestdate.day)
+    sel = [Measurement.date,Measurement.tobs]
+    queryresult = session.query(*sel).filter(Measurement.date >= querydate).all()
+    session.close()
+
+    tobsall = []
+    for date, tobs in queryresult:
+        tobs_dict = {"Date":date,"Tobs":tobs}
+        tobsall.append(tobs_dict)
+
+    return jsonify(tobsall)
+
+@app.route(route_start)
+def get_start(start):
+    session = Session(engine)
+    queryresult = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+        filter(Measurement.date >= start).all()
+    session.close()
+
+    tobsall = []
+    for min,avg,max in queryresult:
+        tobs_dict = {"Min":min,"Average":avg,"Max":max}
+        tobsall.append(tobs_dict)
+
+    return jsonify(tobsall)
 
 @app.route(route_startstop)
+def get_start_stop(start, stop):
+    session = Session(engine)
+    queryresult = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+        filter(Measurement.date >= start).filter(Measurement.date <= stop).all()
+    session.close()
+
+    tobsall = []
+    for min,avg,max in queryresult:
+        tobs_dict = {"Min":min,"Average":avg,"Max":max}
+        tobsall.append(tobs_dict)
+
+    return jsonify(tobsall)
 
 
-
+# Run the Flask application
+if __name__ == '__main__':
+    app.run()
